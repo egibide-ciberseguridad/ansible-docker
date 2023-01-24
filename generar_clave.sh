d5d3718f8070ff92ff68ec1dc58fa6160e03f93a
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2023-01-24 17:18:22 +0100

    Mostrar si generamos una clave nueva

--- generar_clave.sh
+++ generar_clave.sh
@@ -8,14 +8,16 @@ ssh-keyscan "$1" >~/.ssh/known_hosts 2>/dev/null
 
 echo
 if [ -s ~/.ssh/known_hosts ]; then
-  echo "Fichero de claves conocidas actualizado para el host: $1"
+    echo "Fichero de claves conocidas actualizado para el host: $1"
 else
-  echo "ERROR: No se ha podido actualizar el fichero de claves conocidas del host: $1"
+    echo "ERROR: No se ha podido actualizar el fichero de claves conocidas del host: $1"
 fi
 
 if [ ! -f id_rsa ]; then
-  ssh-keygen -b 2048 -t rsa -f id_rsa -q -N ""
-  chmod 400 id_rsa
+    ssh-keygen -b 2048 -t rsa -f id_rsa -q -N ""
+    chmod 400 id_rsa
+    echo
+    echo "Nueva clave privada generada."
 fi
 
 echo
